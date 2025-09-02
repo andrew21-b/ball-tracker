68567b55289812989b5c01c27abe2a40293bd8f4
Andrew Boateng <andrew.21@outlook.com>
2025-09-02 16:00:50 +0100

    Updaates tracker.hpp to detect both coloured balls and black and white

--- include/tracker.hpp
+++ include/tracker.hpp
@@ -2,10 +2,16 @@
 #include <opencv2/opencv.hpp>
 #include <vector>
 
+enum class DetectionMode
+{
+    HSV,
+    GRAYSCALE
+};
+
 class BallTracker
 {
 public:
-    BallTracker();
+    BallTracker(DetectionMode mode = DetectionMode::HSV);
 
     cv::Mat preprocessFrame(const cv::Mat &frame);
     cv::Point2f detectBall(const cv::Mat &mask, cv::Mat &outputFrame);
@@ -13,5 +19,9 @@ public:
     double computeFPS(int64 startTick, int64 endTick);
 
 private:
+    DetectionMode mode;
     std::vector<cv::Point2f> trajectory;
+
+    cv::Point2f detectContours(const cv::Mat& mask, cv::Mat& outputFrame);
+    cv::Point2f detectCircles(const cv::Mat& gray, cv::Mat& outputFrame);
 };
